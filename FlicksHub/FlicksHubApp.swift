e93bcfb8c865f908d3c8f279ddf5ed19c34d6ca5
Lucas Remígio <lucasxavier5435@hotmail.com>
2024-10-01 17:54:20 +0100

    Initial Commit

---
+++ FlicksHub/FlicksHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlicksHubApp.swift
+//  FlicksHub
+//
+//  Created by Lucas Remigio on 01/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct FlicksHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FlicksHubTests/FlicksHubTests.swift
@@ -0,0 +1,17 @@
+//
+//  FlicksHubTests.swift
+//  FlicksHubTests
+//
+//  Created by Lucas Remigio on 01/10/2024.
+//
+
+import Testing
+@testable import FlicksHub
+
+struct FlicksHubTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
